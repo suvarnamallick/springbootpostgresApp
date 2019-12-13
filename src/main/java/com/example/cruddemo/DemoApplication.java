a175eacce47d2cf7250a360d5bdd4921bf3ee057
suvarnamallick <suvarnamallick2030@gmail.com>
2019-12-13 06:35:55 +0530

    initiall

--- src/main/java/com/example/cruddemo/DemoApplication.java
+++ src/main/java/com/example/cruddemo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.cruddemo;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication 
 public class DemoApplication {
-
+	
+	@Value("${server.http.port")
+	private int httpPort;
+	
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
     
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+    	TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+    	tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+    	return tomcat;
+    }
+    
+    private Connector createStandardConnector() {
+    	Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocal");
+    	connector.setPort(httpPort);
+    	return connector;
+    }
 }
